d36eb9d953b2b3512b98a6f77f16828b82b73c34
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-09-27 13:17:32 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
+//% weight=10 color=#1E90FF icon="\uf136"
+namespace HANSHIN_STEM_SENSORS {
+    let buffer = ""
 
+    //% blockId=initSerial block="Init serial port to |TX = %Tx RX=%RX"
+    //% Tx.fieldEditor="gridpicker" Tx.fieldOptions.columns=4
+    //% Rx.fieldEditor="gridpicker" Rx.fieldOptions.columns=4
+    export function initSerial(Tx: SerialPin, Rx: SerialPin): void {
+        serial.redirect(Tx, Rx, 9600)
+        buffer = serial.readString()
+        basic.pause(100)
+        serial.writeString("AT")
+        basic.pause(100)
+    }
+
+    export function setMPU6050Model() : void {
+        
+    }
+}
