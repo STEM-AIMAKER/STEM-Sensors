6c799114aaa5fc80b341b101e4ac3b9d03d41273
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-09-29 14:00:42 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@
 namespace HANSHIN_STEM_SENSORS {
     let buffer = ""
 
+    enum MODE {
+        Active=0,
+        Passive=1
+    }
+
     //% blockId=initSerial block="Init serial port to |TX = %Tx RX=%RX"
     //% Tx.fieldEditor="gridpicker" Tx.fieldOptions.columns=4
     //% Rx.fieldEditor="gridpicker" Rx.fieldOptions.columns=4
@@ -13,7 +18,36 @@ namespace HANSHIN_STEM_SENSORS {
         basic.pause(100)
     }
 
-    export function setMPU6050Model() : void {
-        
+    //% blockId=setMPU6050Model block="Set MPU6050 Model to |mode=%mode active interval time=%activeInterval second"
+    //% activeInterval.min=1 activeInterval.max=9 activeInterval.defl=5
+    export function setMPU6050Model(mode: MODE, activeInterval: number) : void {
+        if( mode === MODE.Active ) {
+            let modeCmd= "ATSPMODE="+activeInterval
+            serial.writeString(modeCmd)
+        }
+        else {
+            serial.writeString("ATSPDATA")
+        }
     }
+
+    //% blockId=queryMPU6050Data block="Query MPU6050 data" 
+    export function queryMPU6050Data(): void {
+        serial.writeString("ATSPDATA")
+    }
+
+
+    export function setGyroModel(mode: MODE, activeInterval: number) : void {
+        if( mode === MODE.Active ) {
+            let modeCmd= "ATGYMODE="+activeInterval
+            serial.writeString(modeCmd)
+        }
+        else {
+            serial.writeString("ATGYDATA")
+        }
+    }
+
+    export function queryGyroData() : void {
+        serial.writeString("ATGYDATA")
+    }
+
 }
